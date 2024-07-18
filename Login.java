5ce192e83e8b7576a27bf302d50a7f8af1de7363
Abduvokhidjon Abdujamolov <abduvokhidjonabdujamolov@AbduvoknsLaptop.hsd1.il.comcast.net>
2024-07-17 20:11:40 -0500

    resolved

--- Login.java
+++ Login.java
@@ -1,7 +1,7 @@
 public class Login {
 	public static void main(String [] args) {
 	System.out.println("Hello World");
-	System.out.prinln("Practicing fetch");
-		System.out.println("Second Practice");
+
+>>>>>>> parent of e83a1f4 (addedPrinlnLine)
 }
 }
