cd4d2d0fcc1b6cbfab4626e26846f1e0685a3dd8
BugHunter <131991957+Abduvokhidjon@users.noreply.github.com>
2024-07-17 19:49:37 -0500

    SecondPractice

--- Login.java
+++ Login.java
@@ -2,5 +2,6 @@ public class Login {
 	public static void main(String [] args) {
 	System.out.println("Hello World");
 	System.out.prinln("Practicing fetch");
+		System.out.println("Second Practice");
 }
 }
